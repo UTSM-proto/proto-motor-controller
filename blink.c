25b6b2ec141da487c0daf84a51a3ac2cc61fb227
brayden-uoft <b.chan.carusone@mail.utoronto.ca>
2026-09-12 14:44:45 -0400

    Lower throttle activation threshold to 1V

--- blink.c
+++ blink.c
@@ -24,7 +24,7 @@ uint8_t hallToMotor[8] = {
 // uint8_t hallToMotor[8] = {255, 2, 0, 1, 4, 3, 5, 255};  // Example hall table
 
 const int THROTTLE_LOW =
-    2000;  // ADC value corresponding to minimum throttle, 0-4095
+    819;  // Minimum throttle: approximately 1.00 V on a 0-5 V input scale
 const int THROTTLE_HIGH =
     3300;  // ADC value corresponding to maximum throttle, 0-4095
 
