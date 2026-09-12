94a7e24263d8a480f62b27a2abae1ddd043828d6
brayden-uoft <b.chan.carusone@mail.utoronto.ca>
2026-09-12 15:41:42 -0400

    Set throttle activation threshold to 0.8V

--- blink.c
+++ blink.c
@@ -24,7 +24,7 @@ uint8_t hallToMotor[8] = {
 // uint8_t hallToMotor[8] = {255, 2, 0, 1, 4, 3, 5, 255};  // Example hall table
 
 const int THROTTLE_LOW =
-    1229;  // Minimum throttle: approximately 1.50 V on a 0-5 V input scale
+    655;  // Minimum throttle: approximately 0.80 V on a 0-5 V input scale
 const int THROTTLE_HIGH =
     3300;  // ADC value corresponding to maximum throttle, 0-4095
 
